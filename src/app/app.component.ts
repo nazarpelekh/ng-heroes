2642d2ff25895ea582db48297463f6d49ef301b5
nazarpelekh <nazar_peleh@meta.ua>
2017-06-26 23:56:08 +0300

    Models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
+import {Hero} from './models/hero';
 
-export class Hero {
-  id: number;
-  name: string;
-}
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
